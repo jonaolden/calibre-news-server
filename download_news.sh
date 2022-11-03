35720dd8b4371463044494d1f24a96be524e1448
Haring, Julius <julius.haring@umlaut.com>
2022-11-03 18:29:00 +0100

    fix download_news.sh

--- download_news.sh
+++ download_news.sh
@@ -2,12 +2,12 @@
 
 for filename in ${RECIPES_FOLDER}/*.recipe; do
     echo "Converting recipe $filename to EPUB $filename.epub"
-    ebook-convert $filename $filename.epub --output-profile=kindle &&
+    ebook-convert $filename $filename.epub --output-profile=kindle
 
     echo "Annotating EPUB $filename.epub with the dailynews\$\$ tag"
-    ebook-meta $filename.epub --tag dailynews&&
+    ebook-meta $filename.epub --tag dailynews
 
     echo "Adding EPUB $filename.epub to the library"
-    calibredb add $filename.epub --library-path "http://127.0.0.1:8080/#calibre" --username "admin" --password "admin"
+    calibredb add $filename.epub --library-path "http://127.0.0.1:8080" --username "admin" --password "admin"
 done
 rm ${RECIPES_FOLDER}/*.epub
