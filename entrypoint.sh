93d93632aa957aa0714d3e914649cc0548eeb8d3
jonaolden <oldenburg.jonatan@gmail.com>
2025-01-15 22:04:27 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+# Run the news download script
 bash /opt/download_news.sh ${LIBRARY_FOLDER}
+
+# Start the Calibre server with authentication
 calibre-server ${LIBRARY_FOLDER} --enable-auth --userdb ${USER_DB}
